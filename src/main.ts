81f88bb3a66282e5bfdcc8a58cb53b7b805c6fe7
motsu8e <motsu159357@gmail.com>
2023-06-04 22:58:05 +0900

    各Path調整

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
-import viteLogo from "/public/vite.svg";
+import viteLogo from "../public/vite.svg";
 import { setupCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+    plugins: [tsconfigPaths()],
+    server: {
+        port: 3000,
+        open: true,
+    },
+    preview: {
+        port: 8000,
+        open: true,
+    },
 })
