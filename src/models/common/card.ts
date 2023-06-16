ff6ffc5ba1841c6e8ef07c37f75afbea1cb46cf8
seiichi0404 <yamayamaseisei1834@gmail.com>
2023-06-16 17:26:44 +0900

    Cardクラスを画像オブジェクトとして扱う

--- src/models/common/card.ts
+++ src/models/common/card.ts
@@ -1,11 +1,26 @@
-export default class Card {
+import Phaser from "phaser";
+
+export default class Card extends Phaser.GameObjects.Image {
   private rank: string;
 
   private suit: string;
 
-  constructor(rank: string, suit: string) {
+  private imagePath: string;
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    rank: string,
+    suit: string,
+    imagePath: string
+  ) {
+    super(scene, x, y, `${suit}${rank}`);
     this.rank = rank;
     this.suit = suit;
+    this.imagePath = imagePath;
+    scene.add.existing(this);
+    this.setScale(0.115);
   }
 
   get getSuit() {
@@ -14,6 +29,14 @@ export default class Card {
 
   get getRank() {
     return this.rank;
+  }
+
+  /**
+   * カードの表面の画像フレーム取得
+   * @returns カードの表面の画像ファイル名
+   */
+  getAtlasFrame(): string {
+    return `card-${this.suit}-${this.rank}.png`;
   }
 
   /**
