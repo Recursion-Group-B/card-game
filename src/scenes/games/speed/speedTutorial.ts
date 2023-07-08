317f5987107e1ad9910a1b685e167763065906c6
motsu8e <motsu159357@gmail.com>
2023-07-08 18:24:29 +0900

    細部修正

--- src/scenes/games/speed/speedTutorial.ts
+++ src/scenes/games/speed/speedTutorial.ts
@@ -1,6 +1,6 @@
 import Tutorial from "../../common/tutorial";
 
-export default class PokerTutorial extends Tutorial {
+export default class SpeedTutorial extends Tutorial {
   preload(): void {
     this.load.image("uTurn", "/public/assets/images/uTurn.svg");
     this.load.video("tutorialVideo", "/public/assets/movies/speedTutorial.mp4", true);
