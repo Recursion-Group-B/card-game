4b4e95649bb3838206062d3b6d7aa3b89611933f
seiichi0404 <yamayamaseisei1834@gmail.com>
2023-07-12 17:36:32 +0900

    warチュートリアル作成

--- src/scenes/games/war/warHelp.ts
+++ src/scenes/games/war/warHelp.ts
@@ -2,7 +2,18 @@ import HelpContainer from "../../common/helpContainer";
 import { helpStyle } from "../../../constants/styles";
 
 const content = `
-war rule
+カジノウォーは、プレイヤーとディーラーが対戦し、より大きな数字を引いた方が勝ちです。
+賭け金をベットし、その後プレイヤー自身とディーラーに1枚ずつカードが配られます。
+
+カードが高い方が勝者となり、賭け金を二倍にして獲得します。
+しかし、もしプレイヤーとディーラーが同じランクのカードを引いた場合、
+プレイヤーは「サレンダー」を選び半分の賭け金を手放すか、
+あるいは「ウォー」を選び同額を再度賭けて再戦を挑むことができます。
+
+このゲームはシンプルかつスピーディーにゲームが進むため、初心者にオススメです。
+
+カードの強さ
+2 < 3 < 4 < 5 < 6 < 7 < 8 < 9 < 10 < J < Q < K < A
 `;
 
 export default class WarHelp extends HelpContainer {
