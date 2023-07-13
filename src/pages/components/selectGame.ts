47c3f901eb792e8ee4dd56e1a561cc331469c6a8
motsu8e <motsu159357@gmail.com>
2023-07-13 21:09:05 +0900

    pagination / navigation を見やすく改善

--- src/pages/components/selectGame.ts
+++ src/pages/components/selectGame.ts
@@ -1,44 +1,46 @@
 const selectGame = `
-<div class="swiper">
-    <div class="swiper-wrapper">
-        <div id="poker" class="swiper-slide">
-            <div class="slider-item">
-                <button class="btn btn-link text-reset" data-game="poker">
-                    <img src="/pokerImg.png" class="img-fit">
-                </button>
-                <h3>Poker</h3>
+<div class="swiper-container">
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <div id="poker" class="swiper-slide">
+                <div class="slider-item">
+                    <button class="btn btn-link text-reset" data-game="poker">
+                        <img src="/pokerImg.png" class="img-fit">
+                    </button>
+                    <h3>Poker</h3>
+                </div>
             </div>
-        </div>
-        <div id="texas" class="swiper-slide">
-            <div class="slider-item">
-                <button class="btn btn-link text-reset" data-game="texas">
-                    <img src="/texasImg.png" class="img-fit">
-                </button>
-                <h3>Texas Hold'em Poker</h3>
+            <div id="texas" class="swiper-slide">
+                <div class="slider-item">
+                    <button class="btn btn-link text-reset" data-game="texas">
+                        <img src="/texasImg.png" class="img-fit">
+                    </button>
+                    <h3>Texas Hold'em Poker</h3>
+                </div>
             </div>
-        </div>
-        <div id="blackjack" class="swiper-slide">
-            <div class="slider-item">
-                <button class="btn btn-link text-reset" data-game="blackjack">
-                    <img src="/blackjackImg.png" class="img-fit">
-                </button>
-                <h3>BlackJack</h3>
+            <div id="blackjack" class="swiper-slide">
+                <div class="slider-item">
+                    <button class="btn btn-link text-reset" data-game="blackjack">
+                        <img src="/blackjackImg.png" class="img-fit">
+                    </button>
+                    <h3>BlackJack</h3>
+                </div>
             </div>
-        </div>
-        <div id="war" class="swiper-slide">
-            <div class="slider-item">
-                <button class="btn btn-link text-reset" data-game="war">
-                    <img src="/warImg.png" class="img-fit">
-                </button>
-                <h3>War</h3>
+            <div id="war" class="swiper-slide">
+                <div class="slider-item">
+                    <button class="btn btn-link text-reset" data-game="war">
+                        <img src="/warImg.png" class="img-fit">
+                    </button>
+                    <h3>War</h3>
+                </div>
             </div>
-        </div>
-        <div id="speed" class="swiper-slide">
-            <div class="slider-item">
-                <button class="btn btn-link text-reset" data-game="speed">
-                    <img src="/speedImg.png" class="img-fit">
-                </button>
-                <h3>Speed</h3>
+            <div id="speed" class="swiper-slide">
+                <div class="slider-item">
+                    <button class="btn btn-link text-reset" data-game="speed">
+                        <img src="/speedImg.png" class="img-fit">
+                    </button>
+                    <h3>Speed</h3>
+                </div>
             </div>
         </div>
     </div>
